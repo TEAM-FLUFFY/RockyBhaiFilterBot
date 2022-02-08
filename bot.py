1ebc7c6d32bbf39ada49734bdfccec2a122663c2
Fluffy <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-02-08 21:00:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
+from Pyrogram import Client, __version__
+from Pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
